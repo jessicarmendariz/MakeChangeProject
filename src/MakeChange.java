19b2076bc807f2b391b558541b78cbe0a3c16a7d
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-07-23 15:50:38 -0600

    Message

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,4 +1,49 @@
+import java.util.Scanner;
 
 public class MakeChange {
+	public static void main(String[] args) {
+		
+		Scanner kb = new Scanner(System.in);
+		double change = 0.00;
+		double price = 0.00;
+		double pay = 0.00;
+		change = pay - price;
+		int counter = 0;
+	
+		
+		System.out.println("What is the price of the item?: ");
+		price = kb.nextDouble();
+		
+		System.out.println("How much money did the customer pay?: ");
+		pay = kb.nextDouble();
+	
+		if (pay < price) {
+			System.err.println("Insufficient Funds.");
+		}
+		
+		if  (pay == price) {
+			System.out.println("Exact Amount. Thank you for shopping!");
+		}
+	
+		
+		
+		if  (pay > price) {
+			System.out.println(change);
+			
+			
+			
+			
+			
+			System.out.println("...still working...");
+		
+		
+		
+		}
+		
+		
+		
+		
+		kb.close();
+	}
 
 }

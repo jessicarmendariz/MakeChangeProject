7c1e48d46dfe6c9ed1c98a113e06392ad0892fce
jessicarmendariz <jessica.n.armendariz@gmail.com>
2021-07-23 16:15:10 -0600

    Currency Breakdown

--- src/MakeChange.java
+++ src/MakeChange.java
@@ -4,12 +4,9 @@ public class MakeChange {
 	public static void main(String[] args) {
 		
 		Scanner kb = new Scanner(System.in);
-		double change = 0.00;
 		double price = 0.00;
 		double pay = 0.00;
-		change = pay - price;
-		int counter = 0;
-	
+		
 		
 		System.out.println("What is the price of the item?: ");
 		price = kb.nextDouble();
@@ -24,26 +21,74 @@ public class MakeChange {
 		if  (pay == price) {
 			System.out.println("Exact Amount. Thank you for shopping!");
 		}
-	
-		
 		
 		if  (pay > price) {
-			System.out.println(change);
+
+			int counter = 0;
+			float change = (float) (pay - price);
 			
+			while (change >= 20) {
+					change -= 20;
+					counter++;
+		}
+			System.out.println(counter + " twenty dollar bill ");
+			counter = 0;
+				
+			while (change >= 10) { 
+					change -= 10;
+					counter ++;
+		}
+			System.out.println(counter + " ten dollar bill");
+			counter = 0;
 			
+			while (change >= 5) {
+					change -= 5;
+					counter ++;
+		}
+			System.out.println(counter + " five dollar bill ");
+			counter = 0;
 			
-			
-			
-			System.out.println("...still working...");
+			while (change >= 1) {
+				change -= 1;
+				counter ++;
+		}
+			System.out.println(counter + " one dollar bill ");
+			counter = 0;
 		
-		
+			while (change >= .25) {
+				change -= .25;
+				counter ++;
+		}
+			System.out.println(counter + " quarters ");
+			counter = 0;
+			
+			while (change >= .10) {
+				change -= .10;
+				counter ++;
+		}
+			System.out.println(counter + " dimes ");
+			counter = 0;
+			
+			while (change >= .05) {
+				change -= .05;
+				counter ++;
+		}
+			System.out.println(counter + " nickels ");
+			counter = 0;
+			
+			while (change >= .01) {
+				change -= .01;
+				counter ++;
+		}
+			System.out.println(counter + " pennies ");
+			counter = 0;
+			
+			
 		
 		}
-		
-		
-		
-		
-		kb.close();
-	}
+
+			kb.close();
+	
+		}
 
 }
